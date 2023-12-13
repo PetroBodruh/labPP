4dfb2a1712ddd59abb7618e547298f24eefa1acf
PetroBodruh <146932058+PetroBodruh@users.noreply.github.com>
2023-12-13 10:13:35 +0200

    lab2

--- src/Customer/Customer.java
+++ src/Customer/Customer.java
@@ -20,6 +20,47 @@ public class Customer {
         this.amount = amount;
     }
 
+    public class RungeKuttaSolver {
+
+        public static void main(String[] args) {
+            // Задані параметри
+            double x0 = 0;  // Початкова точка x
+            double y0 = -3;  // Початкове значення y
+            double h = 0.001;  // Крок
+
+            // Вивід заголовка
+            System.out.println("Step\t x\t\t y\t\t k1\t\t k2\t\t k3\t\t k4");
+
+            // Виклик функції для розв'язання рівняння
+            rungeKutta(x0, y0, h);
+        }
+
+        // Функція, що описує диференціальне рівняння
+        public static double f(double x, double y) {
+            return x * y - Math.cos(Math.pow(x, 2) + 2 * y);
+        }
+
+        // Метод Рунге-Кутта четвертого порядку
+        public static void rungeKutta(double x0, double y0, double h) {
+            double x = x0;
+            double y = y0;
+
+            System.out.printf("%d\t %.4f\t %.4f", 0, x, y);
+
+            for (int i = 1; x < 1; i++) {
+                double k1 = h * f(x, y);
+                double k2 = h * f(x + h / 2, y + k1 / 2);
+                double k3 = h * f(x + h / 2, y + k2 / 2);
+                double k4 = h * f(x + h, y + k3);
+
+                y = y + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+                x = x + h;
+
+                // Вивід значень
+                System.out.printf("\n%d\t %.4f\t %.4f\t %.4f\t %.4f\t %.4f\t %.4f", i, x, y, k1, k2, k3, k4);
+            }
+        }
+    }
 
     public int getCustomerId() {
         return customerId;
